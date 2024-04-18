a4727bc4faeabbe4bceb01cfc4fb412f80350cf4
titoni-andrea <andreatitoni@gmail.com>
2024-04-18 16:15:08 +0200

    Milestone 1 ok

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
-let nomeUtente = document.getElementById("name").value;
-let cognomeUtente = document.getElementById("cognome").value;
-let coloreUtente = document.getElementById("colore").value;
+const bottoneForm = document.getElementById("bottone-form")
+    
+
+bottoneForm.addEventListener("click", function () {
+    let nomeUtente = document.getElementById("name").value;
+    let NumKm = document.getElementById("km").value;
+    let ageUser = document.getElementById("select-eta").value;
+
+    let costKm = 0.21
+
+    let costTicket = NumKm * costKm
+
+    const senzaDato = "Devi inserire tutti i dati nel form per avere il  prezzo del tuo biglietto!"
+
+    // console.log(`Il costo base del biglietto è: ${costTicket.toFixed(2)}€`)
+
+    if (NumKm == 0 || NumKm == null) {
+        console.log(senzaDato)
+    }
+
+    else if (ageUser == 0 || ageUser == null) {
+        console.log(senzaDato)
+    }
+
+    else if (nomeUtente == 0 || nomeUtente == null) {
+        console.log(senzaDato)
+    }
+
+    else if (ageUser == "Minorenne") {
+        console.log(`Il costo del biglietto per te che sei minorenne è: ${(costTicket - ((costTicket / 100) * 20)).toFixed(2)}€`)
+    }
+    else if (ageUser == "Senior") {
+        console.log(`Il costo del biglietto per te che hai più di 65 anni è: ${(costTicket - ((costTicket / 100) * 40)).toFixed(2)}€`)
+    }
+    else {
+        console.log(`Il costo del biglietto intero è: ${costTicket.toFixed(2)}€`)
+    }
+})
+
